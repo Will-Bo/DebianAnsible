dd9fb83aa7bf1f59ea55d5009d45f1dde33edf80
Will-Bo <awill4me@gmail.com>
2023-03-21 10:58:48 -0400

    Removed GUI apps from Nix deployment due to various issues, distro package manager preferred for those

--- nix/experimentalNixDeployment.sh
+++ nix/experimentalNixDeployment.sh
@@ -2,40 +2,24 @@ curl -L https://nixos.org/nix/install | sh
 
 . /home/$USER/.nix-profile/etc/profile.d/nix.sh
 
-nix-channel --add https://nixos.org/channels/nixpkgs-unstable
+nix-channel --add https://nixos.org/channels/nixpkgs-unstable;
 
-nix-channel --update
-nix-env -u '*'
+nix-channel --update;
+nix-env -u '*';
 
 nix-env -iA nixpkgs.helix \
     ## Lots of text editors to choose from
     nixpkgs.kakoune \
-    nixpkgs.git \
     nixpkgs.neovim \
-    ## DEs
-    nixpkgs.hyprland \
-    nixpkgs.i3 \
-    nixpkgs.feh \
+    nixpkgs.git \
     nixpkgs.texlive.combined.scheme-full \
-    nixpkgs.brave \
-    nixpkgs.firefox \
-#    nixpkgs.python3Full \
     nixpkgs.vlc \
-    nixpkgs.dmenu \
-    nixpkgs.picom \
-    nixpkgs.kitty \
     nixpkgs.ranger \
     nixpkgs.anki \
-    nixpkgs.calibre \
     nixpkgs.ffmpeg \
     nixpkgs.pandoc \
-    nixpkgs.clementine \
-    nixpkgs.libsForQt5.soundkonverter \
     nixpkgs.yt-dlp \
-    nixpkgs.transmission \
     nixpkgs.mupdf \
-    ## mesa is hopefully unnecessary, most systems come with this installed already
-#    nixpkgs.mesa \
     nixpkgs.maim \
     ## VSCodium for development
     nixpkgs.vscodium \
@@ -48,15 +32,9 @@ echo 'theme="monokai"' >> ~/.config/helix/config.toml
 
 
 ## Ensuring that all folders necessary for configurations are present
-mkdir -p ~/.config/i3
-mkdir ~/.config/picom
-mkdir ~/Pictures
 mkdir -p ~/.config/nvim/
 
 ## Copying configuration files
-cp ../i3/config ~/.config/i3/config
-cp ../picom/picom.conf ~/.config/picom/picom.conf
-cp ../bg.png ~/Pictures/bg.png
 cp ../50-mouse-acceleration.conf /etc/X11/xorg.conf.d/50-mouse-acceleration.conf
 cp ../.vimrc ~/.vimrc
 
