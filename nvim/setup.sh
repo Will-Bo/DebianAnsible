f0b4e34d0cb643e87e4af04d239623b1ac54017d
will <awill4me@gmail.com>
2023-04-23 15:13:56 -0400

    Fixed typo

--- nvim/setup.sh
+++ nvim/setup.sh
@@ -5,6 +5,6 @@ mkdir ~/.config/nvim
 # From the packer GitHub page
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-cp nvim/* ~/.config/nvim
+cp -r nvim/* ~/.config/nvim
 
 echo "Now go run :PackerSync" from inside NeoVim to install your packages!
