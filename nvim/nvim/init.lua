4a3a10a927d15d566acb783677219c37b220e3ab
Will-Bo <awill4me@gmail.com>
2023-04-23 10:26:35 -0400

    neovim config added

---
+++ nvim/nvim/init.lua
@@ -0,0 +1,11 @@
+require("will.remap")
+require("will.packer")
+
+vim.cmd('colorscheme rose-pine')
+vim.cmd('set rnu')
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.shiftwidth = 4
+vim.opt.expandtab = true
+
