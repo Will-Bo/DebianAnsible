6606db671f99f9d36a61f06d3deb4d22ed549846
Will-Bo <awill4me@gmail.com>
2023-05-03 09:51:08 -0400

    More void tweaks

--- void/setup.sh
+++ void/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo xbps-install wayland-protocols wlroots waybar weston-xwayland xorg-server-xwayland xorg-server-xwayland-dbg
+sudo xbps-install wayland-protocols wlroots waybar weston-xwayland xorg-server-xwayland xorg-server-xwayland-dbg sway pipewire alsa-pipewire lf thunar
 
 git clone https://github.com/djpohly/dwl.git
 
@@ -9,3 +9,11 @@ echo "Now go configure and install dwl!"
 echo "Some patches are here: https://github.com/djpohly/dwl/wiki/Patches"
 
 echo "Like this one! https://github.com/djpohly/dwl/wiki/fibonacci"
+
+echo "For sway use, copy i3 config to ~/.config/sway/config"
+
+mkdir -p ~/.config/i3
+mkdir ~/.config/picom
+mkdir ~/Pictures
+mkdir -p ~/.config/nvim/
+mkdir -p ~/.config/tmux
